d0710fc4596eeb8ebbcdd3cf09d66097dcdfba94
Hari927 <79081714+Hari927@users.noreply.github.com>
2021-02-16 16:43:33 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,10 @@
+## Getting started 
+1.1 about version control
+1.2 history of git
+Windows users
+The binaries for AMD64 will also work on processors that implement the Intel 64 architecture. (Also known as the "x64" architecture, and formerly known as both "EM64T" and "x86-64".)
+There are now "web-based" installers for Windows platforms; the installer will download the needed software components at installation time.
+There are redistributable zip files containing the Windows builds, making it easy to redistribute Python as part of another software package. Please see the documentation regarding Embedded Distribution for more information.
+macOS users
+We provide two binary installer options for download. The default variant is 64-bit-only and works on macOS 10.9 (Mavericks) and later systems. Changed in 3.7.4 The 64-bit/32-bit variant that also works on very old versions of macOS, from 10.6 (Snow Leopard) on, is now deprecated and will no longer be provided in future releases; see the macOS installer ReadMe file for more info. Both variants come with batteries-included versions of Tcl/Tk 8.6 for users of IDLE and other tkinter-based GUI applications; third-party and system versions of Tcl/Tk are no longer used.
+Both python.org installer variants include private copies of OpenSSL. Please carefully read the Important Information displayed during installation for information about SSL/TLS certificate validation and the Install Certificates.command. Changed in 3.7.4 OpenSSL has been updated from 1.1.0 to 1.1.1.
